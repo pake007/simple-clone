a16337def27d9c015a92f7ca0dbe1e97fdfc31d6
Jimmy Huang <pake007@gmail.com>
2013-01-30 14:37:48 +0800

    Version 0.6, published

--- simpleclone-0.6.js
+++ simpleclone-0.6.js
@@ -1,7 +1,36 @@
+/*
+simple_clone v0.6
+Requires jQuery version: >= 1.3.0
+
+Copyright (c) 2011-2013 Jimmy Huang
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 (function($) {
 
-$.fn.simple_clone = function(option){
-  var option = option || {};
+$.fn.simple_clone = function(custom_option){
+  var option = custom_option || {};
 
   var e = $(this);
   e.addClass("left")
